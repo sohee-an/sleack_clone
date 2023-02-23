e82236c2fa86d5a436d0b9380f90d28f4d8a9794
soheee <soan32@hanmail.net>
2023-02-23 18:21:35 +0900

    portupdate

--- sleact_cloneCoding/webpack.config.ts
+++ sleact_cloneCoding/webpack.config.ts
@@ -83,7 +83,7 @@ const config: Configuration = {
     static: { directory: path.resolve(__dirname) },
     proxy: {
       '/api/': {
-        target: 'http://43.201.108.188',
+        target: 'http://localhost:3095',
         changeOrigin: true,
       },
     },
